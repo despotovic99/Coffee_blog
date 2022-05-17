65cda5b6947d502df5b602b57ba74ca146a28c1c
Nemanja <d.neca99@gmail.com>
2022-05-17 19:53:46 +0200

    kreirani kontroleri

--- backend/app/Http/Controllers/CoffeePostCommentController.php
+++ backend/app/Http/Controllers/CoffeePostCommentController.php
@@ -2,9 +2,84 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\CoffeePostComment;
 use Illuminate\Http\Request;
 
 class CoffeePostCommentController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\CoffeePostComment  $coffeePostComment
+     * @return \Illuminate\Http\Response
+     */
+    public function show(CoffeePostComment $coffeePostComment)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\CoffeePostComment  $coffeePostComment
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(CoffeePostComment $coffeePostComment)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\CoffeePostComment  $coffeePostComment
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, CoffeePostComment $coffeePostComment)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\CoffeePostComment  $coffeePostComment
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(CoffeePostComment $coffeePostComment)
+    {
+        //
+    }
 }
--- backend/app/Models/Coffee.php
+++ backend/app/Models/Coffee.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Coffee extends Model
 {
     use HasFactory;
+
+
 }
